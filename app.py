b78a4235ec0e25fd01b1a2822ce09b116f95f5b5
Brian-FullStack <brian.fullstack@gmail.com>
2022-01-03 18:34:37 +0000

    Created Flask App

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import os
+
+
+app = Flask(__name__)
+
+
+app.route("/")
+def test():
+    return "Success!! You Flask app is working correctly!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
