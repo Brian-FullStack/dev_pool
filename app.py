06e3339b1cd864f8a309b61ea2d1100027d05259
Brian-FullStack <brian.fullstack@gmail.com>
2022-01-04 22:40:01 +0000

    Added register template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -25,6 +26,11 @@ mongo = PyMongo(app)
 def get_assets():
     assets = mongo.db.assets.find()
     return render_template('assets.html', assets=assets)
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
