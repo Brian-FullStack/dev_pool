e65619f625cdfeae0f37d1e8858643bff4b4dcd6
Brian-FullStack <brian.fullstack@gmail.com>
2022-01-10 01:24:18 +0000

    Added Card to display assets, added custom styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 /**Code from: https://materializecss.com/navbar.html */
+/** Side navigation*/
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
+  });
+
+/** Tool Tip */
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
   });
