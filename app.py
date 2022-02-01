6130c783cf75a3964ac4704129b08725a6b7a1ae
Brian-FullStack <brian.fullstack@gmail.com>
2022-02-01 00:22:45 +0000

    Added 500 error page

--- app.py
+++ app.py
@@ -207,6 +207,12 @@ def delete_category(category_id):
 def page_not_found(error):
     return render_template("404.html", error=error), 404
 
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template("500.html", error=error), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
